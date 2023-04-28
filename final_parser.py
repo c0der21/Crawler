b9a4608b10e75a5dd00de5b80577e3599a908477
Воронович Кирилл <kirvor21@yandex.ru>
2023-04-28 23:26:46 +0300

    added Semaphore

--- final_parser.py
+++ final_parser.py
@@ -20,17 +20,32 @@ class SiteMapGenerator:
         self.max_requests = max_requests
         self.request_count = 0
         self.output_file = output_file
+        self.semaphore = threading.BoundedSemaphore(num_threads)
+        
+
+        if self.is_valid_url(root_url):
+            link, level = self.q.get()
+            print(link, level)
+            page_content = self.get_html(link)
+            print(page_content)
+            linked_urls = self.get_links(page_content)
+            for url in linked_urls:
+                print(url)
+                if self.is_valid_url(url) and self.has_same_base_url(url) and url not in self.urls_seen \
+                        and self.request_count < self.max_requests:
+                    self.q.put((url, level+1))
+            print(self.q.queue)
     
+
     def generate_sitemap(self):
         '''
         Функция генерации карты сайта, используя разные потоки
         '''
-        threads = []
-        for _ in range(self.num_threads):
-            t = threading.Thread(target=self.worker)
+        threads = [threading.Thread(target=self.worker) for _ in range(self.num_threads)]
+
+        for t in threads:
             t.start()
-            threads.append(t)
-        
+
         for t in threads:
             t.join()
 
@@ -41,7 +56,7 @@ class SiteMapGenerator:
         '''
         Функция записи просмотренных адресов в файл
         '''
-        with open(self.output_file, 'w') as f:
+        with open(self.output_file, 'w', encoding='utf-8') as f:
             for url in self.urls_seen:
                 f.write(url + '\n')
 
@@ -65,6 +80,7 @@ class SiteMapGenerator:
         '''
         Основаня функция скролинга сайта
         '''
+        self.semaphore.acquire()
         while not self.q.empty() and self.request_count < self.max_requests:
             link, level = self.q.get()
             print(link, level)
@@ -92,6 +108,7 @@ class SiteMapGenerator:
                 if self.is_valid_url(url) and self.has_same_base_url(url) and url not in self.urls_seen \
                         and self.request_count < self.max_requests:
                     self.q.put((url, level+1))
+        self.semaphore.release()
 
     def get_html(self, url):
         '''
@@ -128,33 +145,23 @@ if __name__ == '__main__':
     import time
 
     root_urls = [
-        "http://crawler-test.com/", # 3285.7475869283891 секунд
-        "http://google.com/", # 907.213484209533 секунд
-        "https://vk.com", #  3155.8327152729034 секунд
-        "https://dzen.ru", # 2275.303955783921 секунд
-        "https://stackoverflow.com" # 3214.436811685562 секунд
+        "http://crawler-test.com/", # 74.9 секунд
+        "http://google.com/", #  134.7 секунд
+        "https://vk.com", #  464.5 секунд
+        "https://dzen.ru", # 441.8 секунд
+        "https://stackoverflow.com" # 1025.8 секунд
     ]
-    
+
     for root_url in root_urls:
         try:
             domain = urlparse(root_url).netloc
             output_file = f"{domain}_sitemap.txt"
-            sitemap_generator = SiteMapGenerator(root_url, output_file=output_file, max_requests=500, max_depth=6)  
+            sitemap_generator = SiteMapGenerator(root_url, num_threads=50, output_file=output_file, max_requests=600, max_depth=6)  
             start_time = time.time()
             sitemap_generator.generate_sitemap()
             end_time = time.time()
             elapsed_time = end_time - start_time
             print("Время выполнения скрипта: ", elapsed_time, "секунд")
-
         except Exception as Ex:
             print(f"Сайт {root_url} не получилось отсканировать")
             print(Ex)
-
-
-
-
-# Здесь SiteMapGenerator - класс, который представляет генератор карты сайта. При создании экземпляра класса передается корневой URL сайта,
-# максимальная глубина, до которой следует обходить структуру сайта, и количество потоков для обработки.
-# Обход структуры реализован в методе worker. При обходе каждый URL добавляется в множество self.urls_seen, чтобы избежать повторных обращений к нему.
-# Метод get_html получает HTML-код страницы по URL с использованием модуля urllib. 
-# Метод get_links извлекает ссылки из HTML-кода с помощью регулярного выражения и возвращает их в виде генератора. Метод normalize_link преобразует относительные ссылки в аб
